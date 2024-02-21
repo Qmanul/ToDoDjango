1ac03f19597b9ada9eee6c9996e9e3d7ee619a06
Qmanul <mrcrazy123123@gmail.com>
2024-02-21 22:20:39 +0600

    v

--- todoproject/frontend/src/components/App.js
+++ todoproject/frontend/src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./HomePage";
+import { getToDoItems } from ".api/todo";
+
+const loadToDoItems = async () => {
+  const res = await getToDoItems();
+  console.log(res)
+};
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    loadToDoItems();
   }
 
   render() {
